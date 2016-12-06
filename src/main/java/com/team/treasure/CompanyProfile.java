04e0417d627429b9a27b5417d8ef37e8f5af792a
Emmanuel <emmanuelthegrey@gmail.com>
2016-12-06 11:12:40 -0500

    Updated companyProfile class

--- src/main/java/com/team/treasure/CompanyProfile.java
+++ src/main/java/com/team/treasure/CompanyProfile.java
@@ -4,15 +4,80 @@ public class CompanyProfile {
 
 	private String companyID;
 	private String companyName;
+	private String userName;
+	private String password;	
 	private String address;
 	private String mainContact;
 	private String companyPhoneNumber;
-	private String mobileNumber;
 	private String email;
 	private String twitterName;
 
 	public CompanyProfile() {
 		super();
+	}
+
+	public String getCompanyID() {
+		return companyID;
+	}
+
+	public void setCompanyID(String companyID) {
+		this.companyID = companyID;
+	}
+
+	public String getCompanyName() {
+		return companyName;
+	}
+
+	public void setCompanyName(String companyName) {
+		this.companyName = companyName;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getMainContact() {
+		return mainContact;
+	}
+
+	public void setMainContact(String mainContact) {
+		this.mainContact = mainContact;
+	}
+
+	public String getCompanyPhoneNumber() {
+		return companyPhoneNumber;
+	}
+
+	public void setCompanyPhoneNumber(String companyPhoneNumber) {
+		this.companyPhoneNumber = companyPhoneNumber;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
 	}
 
 	public String getTwitterName() {
@@ -22,61 +87,6 @@ public class CompanyProfile {
 	public void setTwitterName(String twitterName) {
 		this.twitterName = twitterName;
 	}
-
-	public String getCompanyID() {
-		return companyID;
-	}
-
-	public String getCompanyName() {
-		return companyName;
-	}
-
-	public String getAddress() {
-		return address;
-	}
-
-	public String getMainContact() {
-		return mainContact;
-	}
-
-	public String getCompanyPhoneNumber() {
-		return companyPhoneNumber;
-	}
-
-	public String getMobileNumber() {
-		return mobileNumber;
-	}
-
-	public String getEmail() {
-		return email;
-	}
-
-	public void setCompanyID(String companyID) {
-		this.companyID = companyID;
-	}
-
-	public void setCompanyName(String companyName) {
-		this.companyName = companyName;
-	}
-
-	public void setAddress(String address) {
-		this.address = address;
-	}
-
-	public void setMainContact(String mainContact) {
-		this.mainContact = mainContact;
-	}
-
-	public void setCompanyPhoneNumber(String companyPhoneNumber) {
-		this.companyPhoneNumber = companyPhoneNumber;
-	}
-
-	public void setMobileNumber(String mobileNumber) {
-		this.mobileNumber = mobileNumber;
-	}
-
-	public void setEmail(String email) {
-		this.email = email;
-	}
-
 }
+
+	
